09f3a24ffad1077c63721f3c8c05a41bab83c0f1
kaiserfaravi <shahidullahkaiserfaravi.com>
2024-02-15 00:21:59 +0600

    zepto-apps-job task  first and last commit done

---
+++ README.md
@@ -0,0 +1,4 @@
+## LIve Site:
+
+ - [link] ()
+ 
---
+++ js/app.js
@@ -0,0 +1,9 @@
+function openClose(){
+    const menuBar = document.getElementById('menu-section');
+
+    if(menuBar.style.display ==='none'){
+        menuBar.style.display ='block'
+    }else{
+        menuBar.style.display ='none'
+    }
+}
